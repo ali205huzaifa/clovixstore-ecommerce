6780f9902d991a7ed3b990494ebd5f3caf19aad2
Ali Huzaifa <alihuzaifa205@gmail.com>
2025-12-01 21:34:32 +0500

    fixes

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useCart, CartItem } from "../components/CartContext";
+import { useCart } from "../context/CartContext";
 
 export default function CartPage() {
   const { cart } = useCart();
@@ -14,15 +14,6 @@ export default function CartPage() {
         <p>Your cart is empty.</p>
       ) : (
         <>
-          <ul className="space-y-4">
-            {cart.map((item: CartItem, i: number) => (
-              <li key={i} className="border p-4 rounded flex justify-between">
-                <span>{item.name}</span>
-                <span>${item.price}</span>
-              </li>
-            ))}
-          </ul>
-
           <Link href="/checkout">
             <button className="mt-6 bg-green-600 text-white px-6 py-2 rounded hover:bg-green-700">
               Proceed to Checkout
